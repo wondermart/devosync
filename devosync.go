677250e1ef65a81322c639e3960990dbb26a100f
Jenny Slaughter <wondermart@gmail.com>
2025-12-08 15:28:41 -0500

    basic implementation of one flag and user input

--- devosync.go
+++ devosync.go
@@ -13,8 +13,30 @@ func main() {
 	cmd := &cli.Command{
 		Name:  "devosync",
 		Usage: "A simple environment sync utility for web developers",
-		Action: func(context.Context, *cli.Command) error {
-			fmt.Println("hey girl")
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "warn",
+				Aliases: []string{"w"},
+				Value:   false,
+				Usage:   "Warn before performing action.",
+			},
+		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+
+			temp := "Test"
+
+			// Check for Arguments.
+			if cmd.NArg() > 0 {
+				temp = cmd.Args().Get(0)
+			}
+
+			// Warning example that doesn't actually let you cancel.
+			if cmd.Bool("warn") {
+				fmt.Println("Warning, I'm going to echo some text at you. Unfortunately, there is nothing you can do to stop it.")
+				fmt.Println(temp)
+			} else {
+				fmt.Println(temp)
+			}
 			return nil
 		},
 	}
